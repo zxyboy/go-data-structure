01b095521bfddd000b8c39a00d4acc9ea2c7be19
zxy <1227387823@qq.com>
2022-07-16 10:07:41 +0800

    完成合并两个链表21题

--- mergetwolists21/MergeTwoLists.go
+++ mergetwolists21/MergeTwoLists.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
 //
@@ -44,50 +46,127 @@ type ListNode struct {
 }
 
 func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	tmpList1 := list1
+	tmpList2 := list2
+
+	if tmpList1 == nil && tmpList2 == nil {
+		return nil
+	}
+	if tmpList1 == nil {
+		return tmpList2
+	}
+
+	if tmpList2 == nil {
+		return tmpList1
+	}
 	var head *ListNode = nil
 	var tail *ListNode = nil
 
-	var n1, n2 int
-	for list1 != nil || list2 != nil {
+	for tmpList1 != nil || tmpList2 != nil {
 
-		if list1 != nil {
-			n1 = list1.Val
-		}
-		if list2 != nil {
-			n2 = list2.Val
+		if tmpList1 == nil {
+			for ; tmpList2 != nil; tmpList2 = tmpList2.Next {
+				node := &ListNode{
+					Val:  tmpList2.Val,
+					Next: nil,
+				}
+				tail.Next = node
+				tail = node
+			}
+			break
 		}
 
+		if tmpList2 == nil {
+			for ; tmpList1 != nil; tmpList1 = tmpList1.Next {
+				node := &ListNode{
+					Val:  tmpList1.Val,
+					Next: nil,
+				}
+				tail.Next = node
+				tail = node
+			}
+			break
+		}
+		n1 := tmpList1.Val
+		n2 := tmpList2.Val
 		if n1 >= n2 {
-			for ; n1 >= n2 && list2 != nil; list2 = list2.Next {
+			node := &ListNode{
+				Val:  n2,
+				Next: nil,
+			}
+			if head == nil {
+				head = node
+				tail = node
+			} else {
+				tail.Next = node
+				tail = node
+			}
+			tmpList2 = tmpList2.Next
 
-				node := &ListNode{
-					Val:  n2,
-					Next: nil,
-				}
-				if head == nil {
-					head = node
-				} else {
-					tail.Next = node
-				}
-				tail = node
-			}
 		} else {
-			for ; n1 < n2 && list1 != nil; list1 = list1.Next {
-				n1 = list1.Val
-				node := &ListNode{
-					Val:  n1,
-					Next: nil,
-				}
-				if head == nil {
-					head = node
-				} else {
-					tail.Next = node
-				}
+			node := &ListNode{
+				Val:  n1,
+				Next: nil,
+			}
+			if head == nil {
+				head = node
+				tail = node
+			} else {
+				tail.Next = node
 				tail = node
 			}
+			tmpList1 = tmpList1.Next
 		}
 	}
 	return head
+
+}
+func mergeTwoLists2(list1 *ListNode, list2 *ListNode) *ListNode {
+	tmpList1 := list1
+	tmpList2 := list2
+
+	if tmpList1 == nil && tmpList2 == nil {
+		return nil
+	}
+	if tmpList1 == nil {
+		return tmpList2
+	}
+
+	if tmpList2 == nil {
+		return tmpList1
+	}
+
+	preHead := &ListNode{
+		Val:  -1,
+		Next: nil,
+	}
+	tail := preHead
+	for tmpList1 != nil || tmpList2 != nil {
+
+		if tmpList1 == nil {
+			tail.Next = tmpList2
+			tmpList2 = nil
+			break
+		}
+
+		if tmpList2 == nil {
+			tail.Next = tmpList1
+			tmpList1 = nil
+			break
+		}
+		n1 := tmpList1.Val
+		n2 := tmpList2.Val
+		if n1 >= n2 {
+			tail.Next = tmpList2
+			tail = tmpList2
+			tmpList2 = tmpList2.Next
+		} else {
+			tail.Next = tmpList1
+			tail = tmpList1
+			tmpList1 = tmpList1.Next
+		}
+	}
+	return preHead.Next
 
 }
 
@@ -115,7 +194,7 @@ func main() {
 		},
 	}
 
-	mergeTwoLists := mergeTwoLists(l1, l2)
+	mergeTwoLists := mergeTwoLists2(l1, l2)
 	for l := mergeTwoLists; l != nil; l = l.Next {
 		fmt.Println(l.Val)
 	}
